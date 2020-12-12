756cd5491ee0a8e524af67bcac057e833a934172
Fantasm4z <fantasm4@outlook.com.br>
2020-12-12 09:49:20 -0300

    Merged SemNome JavaScript Indentation, fixed and optimized code

---
+++ index.js
@@ -0,0 +1,24 @@
+import clipBoarder from 'clipboardy';
+import colorsFun from 'colors';
+
+const indentationFun = async ( ) => {
+
+    const clipboard = await clipBoarder.readSync ( );
+
+    if ( clipboard.length != 0 && clipboard.includes ( `GET` ) || clipboard.includes ( `POST` ) ) {
+        
+        const setUnique = new Set ( clipboard.split ( `\n` ) );
+
+        let arrFilter = [ ...setUnique ].filter ( ( e ) => { return e != ""; } );
+
+        var anotherFilter = arrFilter [ 0 ].match ( 'GET' ) ? anotherFilter = arrFilter.filter ( v => v ).slice ( 1 ) : anotherFilter = arrFilter.filter ( v => v ).slice ( 1 ).slice ( 0, -1 );
+
+        console.log ( `"${ anotherFilter.join ( '\n' ).replaceAll ( ': ', '": "' ).replace ( /\n/g, '", "' ) }"` );
+        //console.log ( '"' + anotherFilter.join('\n').replaceAll(': ', '": "').replace(/\n/g, '", "') + '"' );
+    } else { 
+
+        console.log ( colorsFun.red ( `Fail on indentation... [ Undetected Fiddler RAW Contents ]` ) );
+    }
+}
+
+indentationFun ( );
